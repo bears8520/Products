511b367e5304ab1aa649c5a90af1b7c4f24d8a93
Mike <jiff852@gmail.com>
2019-03-07 05:51:14 +0800

    add load/read file

--- products.py
+++ products.py
@@ -1,13 +1,26 @@
+import os
 products = []
+
+if os.path.isfile('products.csv'): #相對路徑/絕對路徑
+	print('有找到檔案')
+	#讀取檔案
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #繼續
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('無清單檔案')
+
+
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q':
 		break
 	price = int(input('請輸入此商品金額:'))
-	p = []
-	p.append(name)
-	p.append(price)
-	products.append(p)
+	products.append([name, price])
 print(products)
 
 for p in products:
